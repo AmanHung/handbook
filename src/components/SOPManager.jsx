ec60dc382118f8b23722dfb1813a5d1239dce75e
AmanHung <obm0304@gmail.com>
2026-01-30 09:32:32 +0800

    Update SOPManager.jsx

--- src/components/SOPManager.jsx
+++ src/components/SOPManager.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { collection, onSnapshot, query, orderBy } from 'firebase/firestore';
-import { db } from '../firebase';
+import { db } from '../firebase.js';
 
 const SOPManager = () => {
   const [searchTerm, setSearchTerm] = useState('');
